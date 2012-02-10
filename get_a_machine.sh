17eacfa621636659d7cc8877d20133b333ec3a6f
Martin Quinson (nancy) <martin.quinson@loria.fr>
2012-02-10 13:38:28 +0100

    make it clearer that this file is not to be executed

--- get_a_machine.sh
+++ get_a_machine.sh
@@ -1,12 +1,23 @@
+echo "this file is not intended to be executed directly."
+echo "instead, pick lines one by one and copy them (after adapting) into your terminal"
+echo
+echo "It seems impossible to improve this, since most of these commands start interactive subshells..."
+exit 1
+
+# Survive ADSL deconnexions
 dtach -A /tmp/mquinson-dtach-socket bash
+
+# Get a bunch of machines
 oarsub -I -l 'nodes=2,walltime=10' -t deploy
 oarsub -I -l 'nodes=1,walltime=10' -p "cluster='parapluie'" -t deploy
+
+# Gain root privilege on them
 kadeploy3 -e squeeze-x64-nfs -f $OAR_NODE_FILE -k ~/.ssh/id_rsa.pub
 
-for node in `cat $OAR_NODE_FILE|sort -u` ; do ssh root@$node "sh ~mquinson/skampi/tune_machine.sh" ; done
-cp $OAR_NODE_FILE ~/tmp/oar-node-file
-sort -u $OAR_NODE_FILE > ~/tmp/hostfile
+# Install the stuff that we need
+~/SMPI-modeling/tune_all_machines.sh
 
+# Run stuff
 mpirun --mca btl self,tcp -machinefile ~/tmp/hostfile BLA
 
 for n in `sort -u $OAR_NODE_FILE` ; do ssh $n "rm /tmp/mq-dtach;killall testall.sh java time run.gridsim;sleep 1; kill -KILL -1" ; done; killall ssh
